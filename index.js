f63e94d35f07ff9ab7681af1b6e9ea10a83ba701
guojunlong <guojunlong@tuniu.com>
2017-03-20 12:36:27 +0800

    fix bugs

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ module.exports = function(defaultRenderHandler) {
         htmlRender: function(req, res, next) {
             let HtmlResponse = common.HtmlResponse;
             let renderHandler = _.merge({}, defaultRenderHandler);
-            res.htmlRender = function(data, layout, viewPath) {
+            res.htmlRender = function(data, layout, viewPath, callback) {
+                let cbFunc = callback || viewPath || layout;
+                if (typeof cbFunc !== 'function') cbFunc = null;
+
+                if (layout === undefined) layout = true;
                 let dataRes = new HtmlResponse();
                 dataRes.viewPath = res.locals.__viewPath__;
                 if (data && data.constructor && data.constructor.name === 'HtmlResponse') { // 忽略参数2、参数3
@@ -39,7 +43,7 @@ module.exports = function(defaultRenderHandler) {
                     }
                 }
                 dataRes.merge(); // 合并结果到result属性.
-                res.render(dataRes.viewPath, dataRes.result);
+                res.render(dataRes.viewPath, dataRes.result, cbFunc);
             };
             next();
         },
